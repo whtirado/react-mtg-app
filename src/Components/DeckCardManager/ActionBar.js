09d4205d12a1f512a21f8e3e60ba7b77b59a234d
whtirado <whtirado@gmail.com>
2020-06-23 05:24:21 -0400

    Action bar and Deck name/count is now sticky.

--- src/Components/DeckCardManager/ActionBar.js
+++ src/Components/DeckCardManager/ActionBar.js
@@ -33,35 +33,37 @@ export default function ActionBar({
         />
       )}
 
-      <div className='flex justify-between shadow p-2 sticky top-0 bg-white'>
-        <Link
-          to='/deck-list-manager'
-          className='flex items-center text-indigo-700'
-        >
-          <FontAwesomeIcon icon={faChevronLeft} />
-          <span className='ml-1'>Back to Decks</span>
-        </Link>
-        <button
-          onClick={() => {
-            setShowModalDeleteDeck(true);
-          }}
-          className='bg-red-800 shadow font-bold py-1 px-2 ml-auto rounded text-white hover:shadow-md hover:bg-red-900 hover:text-red-100'
-        >
-          Delete deck
-        </button>
-        {clearDeck && (
+      <div className='sticky top-0 bg-white border-b border-indigo-700 mb-2'>
+        <div className='flex justify-between shadow p-2'>
+          <Link
+            to='/deck-list-manager'
+            className='flex items-center text-indigo-700'
+          >
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <span className='ml-1'>Back to Decks</span>
+          </Link>
           <button
             onClick={() => {
-              setShowModalClearDeck(true);
+              setShowModalDeleteDeck(true);
             }}
-            className='bg-red-800 shadow font-bold py-1 px-2 ml-1 rounded text-white hover:shadow-md hover:bg-red-900 hover:text-red-100'
+            className='bg-red-800 shadow font-bold py-1 px-2 ml-auto rounded text-white hover:shadow-md hover:bg-red-900 hover:text-red-100'
           >
-            Clear deck
+            Delete deck
           </button>
-        )}
-      </div>
-      <div className='font-bold text-lg py-2 text-indigo-700'>
-        {deckName}: {deckCardLength} card(s)
+          {clearDeck && (
+            <button
+              onClick={() => {
+                setShowModalClearDeck(true);
+              }}
+              className='bg-red-800 shadow font-bold py-1 px-2 ml-1 rounded text-white hover:shadow-md hover:bg-red-900 hover:text-red-100'
+            >
+              Clear deck
+            </button>
+          )}
+        </div>
+        <div className='font-bold text-lg py-2 text-indigo-700'>
+          {deckName}: {deckCardLength} card(s)
+        </div>
       </div>
     </>
   );
