46208fab23f5a15494a97d87c75557947dcf9a9b
whtirado <whtirado@gmail.com>
2020-06-28 23:20:08 -0400

    Replaced parent elements with react frgments.

--- src/Components/DeckListManager/js/Decks.js
+++ src/Components/DeckListManager/js/Decks.js
@@ -5,21 +5,17 @@ export default function Decks({ decks }) {
   let content = null;
 
   if (decks.length) {
-    content = (
-      <div className=''>
-        {decks.map((deck) => {
-          return (
-            <Link
-              key={deck.name}
-              to={`/deck-card-manager/${deck.name}`}
-              className='block border-2 border-indigo-700 shadow rounded p-2 mb-2 hover:bg-indigo-700 hover:border-indigo-900 hover:shadow-md hover:text-indigo-100'
-            >
-              {deck.name}
-            </Link>
-          );
-        })}
-      </div>
-    );
+    content = decks.map((deck) => {
+      return (
+        <Link
+          key={deck.name}
+          to={`/deck-card-manager/${deck.name}`}
+          className='block border-2 border-indigo-700 shadow rounded p-2 mb-2 hover:bg-indigo-700 hover:border-indigo-900 hover:shadow-md hover:text-indigo-100'
+        >
+          {deck.name}
+        </Link>
+      );
+    });
   } else {
     content = <span>No decks found. Click "New" to create a new deck.</span>;
   }
