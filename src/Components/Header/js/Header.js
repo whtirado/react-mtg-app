484620ef1ea2e675f17d6ee851abe1a364d13ee6
whtirado <whtirado@gmail.com>
2020-06-28 22:26:22 -0400

    Cleaned up SearchCardDetail import.

--- src/Components/Header/js/Header.js
+++ src/Components/Header/js/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SearchCardDetail from '../../SearchCardDetail/js/SearchCardDetail';
+import SearchCardDetail from '../../SearchCardDetail';
 
 export default function Header() {
   return (
